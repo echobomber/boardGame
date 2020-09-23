5cb0f9ac165d67053e9b1eff55dba855ec82f3e7
echobomber <jordan990301@g-mail.nsysu.edu.tw>
2020-09-24 00:02:54 +0800

    Initial commit

---
+++ js/all.js
@@ -0,0 +1,19 @@
+// $('.header-menu2').css('top', '-100%');
+// $(window).scroll(function(){
+//     let scrollPos = $(window).scrollTop();
+//     let windowHeight = $(window).height();
+//     let targetPos = $('#part1').offset().top;
+//     if(scrollPos + 100 >= targetPos){
+//         $('.header-menu2').css('top', '0');
+//     }else{
+//         $('.header-menu2').css('top', '-100%');
+//     }
+// })
+$('.menu-open').on('click', function(e){
+    e.preventDefault();
+    $('.menu-overlay').css('height', '100%');
+})
+$('.menu-close').on('click', function(e){
+    e.preventDefault();
+    $('.menu-overlay').css('height', '0');
+})
