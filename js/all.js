8b91c47168bfc527a2869fde7d055f620ea41a70
echobomber <jordan990301@g-mail.nsysu.edu.tw>
2020-09-26 18:31:37 +0800

    分享按鈕測試

--- js/all.js
+++ js/all.js
@@ -17,3 +17,7 @@ $('.menu-close').on('click', function(e){
     e.preventDefault();
     $('.menu-overlay').css('height', '0');
 })
+$('.fb-share').on('click', function(e){
+    e.preventDefault();
+    document.querySelector('.fb-share-button').click();
+})
